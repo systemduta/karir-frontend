04c128ae6e405b709252dd144c3ec91f9f3db841
Micko Widi <widimicko85@gmail.com>
2022-04-23 13:25:11 +0700

    create footer

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,205 @@
+<template>
+  <div class="bg-primary py-3">
+    <div
+      class="d-flex gap-4 align-items-center"
+      style="overflow-x: auto; height: 150px"
+    >
+      <a href="#" target="_blank" rel="noopener noreferrer">
+        <img
+          src="@/assets/companies/Logo-Bank-Arthaya.png"
+          class=""
+          height="50px"
+          alt="Bank Arthaya"
+        />
+      </a>
+      <a href="#" target="_blank" rel="noopener noreferrer">
+        <img
+          src="@/assets/companies/Logo-CUN.png"
+          class=""
+          height="50px"
+          alt="CUN Motor"
+        />
+      </a>
+      <a href="#" target="_blank" rel="noopener noreferrer">
+        <img
+          src="@/assets/companies/Logo-AUM.png"
+          class=""
+          height="50px"
+          alt="AUM"
+        />
+      </a>
+      <a href="#" target="_blank" rel="noopener noreferrer">
+        <img
+          src="@/assets/companies/Logo-Prama-Logistic.png"
+          class=""
+          height="50px"
+          alt="Prama Logistic"
+        />
+      </a>
+      <a href="#" target="_blank" rel="noopener noreferrer">
+        <img
+          src="@/assets/companies/Logo-Maesa-Hotel.png"
+          class=""
+          height="50px"
+          alt="Maesa Hotel"
+        />
+      </a>
+      <a href="#" target="_blank" rel="noopener noreferrer">
+        <img
+          src="@/assets/companies/Logo-Pabrik-Es-PMP.png"
+          class=""
+          height="50px"
+          alt="PMP Ice"
+        />
+      </a>
+      <a href="#" target="_blank" rel="noopener noreferrer">
+        <img
+          src="@/assets/companies/Logo-MIT.png"
+          class=""
+          height="50px"
+          alt="MIT"
+        />
+      </a>
+      <a href="#" target="_blank" rel="noopener noreferrer">
+        <img
+          src="@/assets/companies/Logo-Pandu-Distributor.png"
+          class=""
+          height="50px"
+          alt="Pandu Distributor"
+        />
+      </a>
+      <a href="#" target="_blank" rel="noopener noreferrer">
+        <img
+          src="@/assets/companies/Logo-Hennessey.png"
+          class=""
+          height="50px"
+          alt="Hennessey"
+        />
+      </a>
+      <a href="#" target="_blank" rel="noopener noreferrer">
+        <img
+          src="@/assets/companies/Logo-Koperasi-SDM.png"
+          class=""
+          height="50px"
+          alt="Koperasi SDM"
+        />
+      </a>
+      <a href="#" target="_blank" rel="noopener noreferrer">
+        <img
+          src="@/assets/companies/Logo-Werkst.png"
+          class=""
+          height="50px"
+          alt="Werkst"
+        />
+      </a>
+      <a href="#" target="_blank" rel="noopener noreferrer">
+        <img
+          src="@/assets/companies/Logo-Es-Kristal-PMP.png"
+          class=""
+          height="50px"
+          alt="PMP Es Kristal"
+        />
+      </a>
+      <a href="#" target="_blank" rel="noopener noreferrer">
+        <img
+          src="@/assets/companies/Logo-Dua-Tangan.png"
+          class=""
+          height="50px"
+          alt="Dua Tangan"
+        />
+      </a>
+    </div>
+  </div>
+  <footer class="text-white" id="footer" style="background: #212121">
+    <div class="row g-4 p-3">
+      <div class="col-12 col-md-4 text-lg-start flex-fill">
+        <img class="img-fluid mb-3" src="@/assets/logo.png" alt="EL logo" />
+        <p>
+          "Menjadi perusahaan yang disegani dalam berbagai bidang usaha dan
+          berdampak positif bagi pemegang saham, lingkungan sekitar dan bangsa"
+        </p>
+        <div class="d-flex gap-3 fs-3">
+          <a
+            href="#"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="text-white"
+            ><i class="bi bi-facebook"></i
+          ></a>
+          <a
+            href="#"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="text-white"
+            ><i class="bi bi-whatsapp"></i
+          ></a>
+          <a
+            href="#"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="text-white"
+            ><i class="bi bi-instagram"></i
+          ></a>
+          <a
+            href="#"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="text-white"
+            ><i class="bi bi-linkedin"></i
+          ></a>
+          <a
+            href="#"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="text-white"
+            ><i class="bi bi-youtube"></i
+          ></a>
+          <a
+            href="#"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="text-white"
+            ><i class="bi bi-tiktok"></i
+          ></a>
+        </div>
+      </div>
+      <div class="col-12 col-md-4">
+        <h5 class="fw-bold text-white">Quicklinks</h5>
+        <ul>
+          <li>
+            <a href="#" class="text-white text-decoration-none">Situs Utama</a>
+          </li>
+          <li>
+            <a href="#" class="text-white text-decoration-none">Peta Situs</a>
+          </li>
+          <li>
+            <a href="#" class="text-white text-decoration-none"
+              >Maesa Foundation</a
+            >
+          </li>
+        </ul>
+      </div>
+      <div class="col-12 col-md-4">
+        <h5 class="fw-bold text-white">Contact Us</h5>
+        <p>
+          62 821-2366-6647 info@maesagroup.co.id Jl. Urip Sumoharjo No.63 Kota
+          Madiun 63127 Jawa Timur - Indonesia
+        </p>
+      </div>
+    </div>
+    <hr />
+    <div id="footer-bottom" class="py-2" style="background: #000000">
+      <div class="container py-2">
+        <div class="row justify-content-between align-items-center">
+          <div class="col-12 col-lg-6 text-center text-md-start">
+            All Right Reserved | PT. Maesa Cipta Corporindo. 2022
+          </div>
+          <div class="col-12 col-lg-6 text-center text-md-end">
+            Privacy Policy &emsp; Site
+            <span class="text-primary text-decoration-underline">Credit</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
