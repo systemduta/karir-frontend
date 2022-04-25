a5c6ad9a66e96219453d98ad3ce5f8589cf893b1
Micko Widi <widimicko85@gmail.com>
2022-04-25 12:31:07 +0700

    split home filter component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import NavigationBar from "@/components/NavigationBar.vue";
 import FooterComponent from "@/components/FooterComponent.vue";
+import FilterComponent from "@/components/FilterComponent.vue";
 </script>
 
 <style scoped>
@@ -66,38 +67,7 @@ import FooterComponent from "@/components/FooterComponent.vue";
           </div>
           <div class="w-full" style="height: 100px"></div>
         </div>
-        <div class="row justify-content-center" id="filterContainer">
-          <div
-            class="shadow rounded-pill text-center p-4 w-75"
-            style="background-color: #e1e1e1; position: absolute; bottom: 0"
-            id="filter"
-          >
-            <form class="row g-3 justify-content-center align-items-center">
-              <div class="col-12 col-md-5">
-                <input
-                  type="text"
-                  name="search"
-                  placeholder="Job title or keyword"
-                  class="form-control rounded-pill"
-                />
-              </div>
-              <div class="col-12 col-md-5">
-                <select name="category" class="form-select rounded-pill">
-                  <option value="">Filter</option>
-                </select>
-              </div>
-              <div class="col-12 col-md-2">
-                <button
-                  type="submit"
-                  class="rounded-pill text-white w-100"
-                  style="background-color: #434343"
-                >
-                  Search
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
+        <FilterComponent />
       </section>
       <div class="w-full" style="height: 250px"></div>
     </main>
@@ -107,21 +77,6 @@ import FooterComponent from "@/components/FooterComponent.vue";
 
 <script>
 export default {
-  mounted: function () {
-    const observer = new ResizeObserver((entries) => {
-      entries.forEach((entry) => {
-        const width = entry.contentRect.width;
-        const filter = document.getElementById("filter");
-        if (width < 772) {
-          filter.classList.remove("rounded-pill");
-          filter.classList.add("rounded-3");
-        } else {
-          filter.classList.add("rounded-pill");
-          filter.classList.remove("rounded-3");
-        }
-      });
-    });
-    observer.observe(document.getElementById("filterContainer"));
-  },
+  mounted: function () {},
 };
 </script>
