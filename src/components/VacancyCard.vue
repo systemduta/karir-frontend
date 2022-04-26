2419fec347f5984a057a3cd0f0bb9afc84cf869d
Micko Widi <widimicko85@gmail.com>
2022-04-26 16:55:14 +0700

    create vacancy card component

---
+++ src/components/VacancyCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="col-12 col-md-6 col-lg-3">
+    <div class="card shadow" style="border-radius: 20px 20px 0 0">
+      <div
+        class="position-absolute bg-primary px-4 py-1"
+        style="top: 0px; right: 0px; border-radius: 0 20px 0 20px"
+      >
+        <p class="fw-bold">Intern</p>
+      </div>
+      <img
+        src="https://maesagroup.co.id/wp-content/uploads/2022/03/8-Frontend-Developer-1024x1024.png"
+        class="card-img-top"
+        style="border-radius: 20px"
+      />
+      <div class="card-body bg-primary">
+        <h5 class="card-title">
+          <a class="text-decoration-none text-white fw-bolder">Programmer</a>
+        </h5>
+      </div>
+    </div>
+  </div>
+</template>
