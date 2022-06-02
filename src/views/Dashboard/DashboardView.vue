2020324ad41def3ee70a4d4dbf838f3f98301f02
Micko Widi <widimicko85@gmail.com>
2022-06-02 15:34:32 +0700

    add bg to dashboard cards

--- src/views/Dashboard/DashboardView.vue
+++ src/views/Dashboard/DashboardView.vue
@@ -3,45 +3,68 @@ import Header from "@/components/Dashboard/HeaderComponent.vue";
 import Sidebar from "@/components/Dashboard/SidebarComponent.vue";
 </script>
 
+<style scoped>
+.statistic-card {
+  border-radius: 10px;
+}
+
+.statistic-card-title {
+  opacity: 0.8;
+}
+
+.statistic-card1 {
+  background-image: url("@/assets/images/vectors/statistic-yellow-1.png");
+}
+
+.statistic-card2 {
+  background-image: url("@/assets/images/vectors/statistic-yellow-2.png");
+}
+
+.statistic-card3 {
+  background-image: url("@/assets/images/vectors/statistic-red.png");
+}
+</style>
+
 <template>
   <div class="d-flex">
     <Sidebar />
     <div class="w-100 bg-grey p-5">
       <div class="container">
         <Header title="Dashboard" />
-        <div class="">
+        <div>
           <p class="fs-4">Statistic</p>
           <div class="d-flex flex-wrap gap-5">
             <div
-              class="card rounded p-3 text-white"
-              style="width: 250px; height: 90px; background-color: #e79317"
+              class="card p-3 text-white statistic-card statistic-card1"
+              style="width: 250px; height: 90px"
             >
-              <p>Lowongan</p>
-              <p>27</p>
+              <p class="statistic-card-title">Lowongan</p>
+              <p class="fw-bold">27</p>
             </div>
             <div
-              class="card rounded p-3 text-white"
-              style="width: 250px; height: 90px; background-color: #e79317"
+              class="card p-3 text-white statistic-card statistic-card2"
+              style="width: 250px; height: 90px"
             >
-              <p>Intern</p>
-              <p>544</p>
+              <p class="statistic-card-title">Intern</p>
+              <p class="fw-bold">544</p>
             </div>
             <div
-              class="card rounded p-3 text-white"
-              style="width: 250px; height: 90px; background-color: #ec1808"
+              class="card p-3 text-white statistic-card statistic-card3"
+              style="width: 250px; height: 90px"
             >
-              <p>Fulltime</p>
-              <p>1,216</p>
+              <p class="statistic-card-title">Fulltime</p>
+              <p class="fw-bold">1,216</p>
             </div>
             <div
-              class="card rounded p-3 text-white"
-              style="width: 250px; height: 90px; background-color: #ec1808"
+              class="card p-3 text-white statistic-card statistic-card3"
+              style="width: 250px; height: 90px"
             >
-              <p>Pelamar</p>
-              <p>4,216</p>
+              <p class="statistic-card-title">Pelamar</p>
+              <p class="fw-bold">4,216</p>
             </div>
           </div>
         </div>
+        <div class="d-flex flex-wrap gap-5"></div>
       </div>
     </div>
   </div>
