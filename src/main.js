aed336077da034d97865830a5a668b478207a957
Micko Widi <widimicko85@gmail.com>
2022-06-09 14:20:41 +0700

    add sweetalert2

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from "vue";
+import VueSweetalert2 from "vue-sweetalert2";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "datatables.net-bs5/css/dataTables.bootstrap5.min.css";
+import "sweetalert2/dist/sweetalert2.min.css";
 
 import "./index.css";
 
@@ -21,5 +23,7 @@ if (token) {
 
 store
   .dispatch("getUser", token)
-  .then(() => createApp(App).use(router).use(store).mount("#app"))
+  .then(() =>
+    createApp(App).use(router).use(store).use(VueSweetalert2).mount("#app")
+  )
   .catch((error) => console.log(error));
