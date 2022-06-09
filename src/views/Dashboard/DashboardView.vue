d651bc9402d9bc3c8116ed83f1d11693f56c9b77
Micko Widi <widimicko85@gmail.com>
2022-06-09 13:48:05 +0700

    add statistic data

--- src/views/Dashboard/DashboardView.vue
+++ src/views/Dashboard/DashboardView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Header from "@/components/Dashboard/HeaderComponent.vue";
 import Sidebar from "@/components/Dashboard/SidebarComponent.vue";
+import LoadingComponent from "@/components/LoadingComponent.vue";
 </script>
 
 <style scoped>
@@ -33,34 +34,51 @@ import Sidebar from "@/components/Dashboard/SidebarComponent.vue";
         <Header title="Dashboard" />
         <div>
           <p class="fs-4">Statistic</p>
-          <div class="d-flex flex-wrap gap-5">
+          <div v-if="loading" class="my-4 d-flex justify-content-center">
+            <LoadingComponent />
+          </div>
+          <div
+            v-if="error"
+            class="alert alert-danger alert-dismissible fade show"
+            role="alert"
+          >
+            {{ error }}
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="alert"
+              aria-label="Close"
+              @click="error = false"
+            ></button>
+          </div>
+          <div class="d-flex flex-wrap gap-5 justify-content-center">
             <div
               class="card p-3 text-white statistic-card statistic-card1"
               style="width: 250px; height: 90px"
             >
               <p class="statistic-card-title">Lowongan</p>
-              <p class="fw-bold">27</p>
+              <p class="fw-bold">{{ data.recruitment }}</p>
             </div>
             <div
               class="card p-3 text-white statistic-card statistic-card2"
               style="width: 250px; height: 90px"
             >
-              <p class="statistic-card-title">Intern</p>
-              <p class="fw-bold">544</p>
+              <p class="statistic-card-title">Total Pelamar</p>
+              <p class="fw-bold">{{ data.participant }}</p>
             </div>
             <div
               class="card p-3 text-white statistic-card statistic-card3"
               style="width: 250px; height: 90px"
             >
-              <p class="statistic-card-title">Fulltime</p>
-              <p class="fw-bold">1,216</p>
+              <p class="statistic-card-title">Pelamar Fulltime</p>
+              <p class="fw-bold">{{ data.fulltime }}</p>
             </div>
             <div
               class="card p-3 text-white statistic-card statistic-card3"
               style="width: 250px; height: 90px"
             >
-              <p class="statistic-card-title">Pelamar</p>
-              <p class="fw-bold">4,216</p>
+              <p class="statistic-card-title">Pelamar Intern</p>
+              <p class="fw-bold">{{ data.internship }}</p>
             </div>
           </div>
         </div>
@@ -69,3 +87,35 @@ import Sidebar from "@/components/Dashboard/SidebarComponent.vue";
     </div>
   </div>
 </template>
+
+<script>
+import { ref } from "vue";
+import axios from "../../plugins/axios";
+
+export default {
+  data() {
+    return {
+      data: ref({}),
+      loading: ref(false),
+      error: ref(false),
+    };
+  },
+  methods: {
+    async fetchDashboardData() {
+      try {
+        this.loading = true;
+        const { data } = await axios.get("dashboard");
+        this.data = data;
+        this.error = false;
+        this.loading = false;
+      } catch (error) {
+        this.loading = false;
+        this.error = true;
+      }
+    },
+  },
+  mounted() {
+    this.fetchDashboardData();
+  },
+};
+</script>
